4f700515c3e79e122df99b22601fd8dab811a7f2
Thomas Zachariah <tzachari@umich.edu>
2016-03-14 00:00:22 -0400

    Make BLE light UI OS-agnostic

--- examples/cordova-apps/light/www/js/index.js
+++ examples/cordova-apps/light/www/js/index.js
@@ -24,8 +24,8 @@
 var deviceID = "";
 
 var light = {
-    service: 'FF10',                                                                                        // light service uuid
-    onoff: 'FF11',                                                                                          // light switch characteristic uuid
+    service: 'FF10',                                                                                              // light service uuid
+    onoff: 'FF11',                                                                                                // light switch characteristic uuid
 };
 
 var app = {
@@ -36,50 +36,50 @@ var app = {
         document.addEventListener('deviceready', this.onDeviceReady, false);
         document.addEventListener('pause', this.onPause, false);
         document.addEventListener('resume', this.onDeviceReady, false);
-        bulb.addEventListener('touchstart', this.onToggle, false);                                          // if bulb image touched, goto: onToggle
+        document.querySelector("svg").addEventListener('touchstart', this.onToggle, false);                       // if bulb image touched, goto: onToggle
     },
     onDeviceReady: function() {
-        deviceID = window.gateway.getDeviceId();                                                            // get device ID from Gateway
-        deviceName = window.gateway.getDeviceName();                                                        // get device name from Gateway
-        document.querySelector("img").className = "Dis";                                                    // bulb image in 'disconnected' mode
-        ble.isEnabled(app.onEnable,function(){                                                              // if BLE enabled, goto: onEnable
-            document.querySelector("#console").innerHTML = "Bluetooth Off.";                                // else, alert user
+        deviceID = window.gateway.getDeviceId();                                                                  // get device ID from Gateway
+        deviceName = window.gateway.getDeviceName();                                                              // get device name from Gateway
+        document.querySelector("svg").setAttribute("class","Dis");                                                // bulb image in 'disconnected' mode
+        ble.isEnabled(app.onEnable,function(){                                                                    // if BLE enabled, goto: onEnable
+            document.querySelector("#console").innerHTML = "Bluetooth Off.";                                      // else, alert user
         });
     },
     onEnable: function() {
         document.querySelector("#console").innerHTML = "Searching...";
-        ble.stopScan();
-        ble.startScan([], app.onDiscover, app.onDeviceReady);                                               // start BLE scan; if device discovered, goto: onDiscover
+        ble.stopScan(function(){ble.startScan([], app.onDiscover, app.onDeviceReady);},app.onDeviceReady);        // start BLE scan; if device discovered, goto: onDiscover
     },
     onDiscover: function(device) {
-        if (device.id == deviceID) {
-            document.querySelector("#console").innerHTML = "Found! Connecting...";
-            ble.connect(deviceID, app.onConnect, app.onDeviceReady);                                        // if device matches, connect; if connected, goto: onConnect
+        if (device.name.startsWith("SHL")) {
+            deviceID=device.id;
+            document.querySelector("#console").innerHTML = "Connecting...";
+            ble.stopScan(function(){ble.connect(deviceID, app.onConnect, app.onDeviceReady);},app.onDeviceReady); // if device matches, connect; if connected, goto: onConnect
         }
     },
     onConnect: function(device) {
-        document.querySelector("#console").innerHTML = "Connected! Syncing...";
-        ble.read(deviceID, light.service, light.onoff, app.onRead, app.onRWError);                          // read switch characteristic; if read is good, goto: onRead 
+        document.querySelector("#console").innerHTML = "Syncing...";
+        ble.read(deviceID, light.service, light.onoff, app.onRead, app.onRWError);                                // read switch characteristic; if read is good, goto: onRead 
     },
     onRead: function(data) {
-        document.querySelector("img").className = ((new Uint8Array(data))[0]===0x1) ? "On" : "Off";         // bulb image in 'on' or 'off' mode
-        document.querySelector("#console").innerHTML = document.querySelector("img").className;             // display text: "On" or "Off"
+        document.querySelector("svg").setAttribute("class",((new Uint8Array(data))[0]===0x1) ? "On" : "Off");     // bulb image in 'on' or 'off' mode
+        document.querySelector("#console").innerHTML = document.querySelector("svg").getAttribute("class");       // display text: "On" or "Off"
     },
     onToggle: function(event) {
-        if (event.target.className!="Dis") {                                                                // if image touched & bulb connected,
+        if (document.querySelector("svg").getAttribute("class")!="Dis") {                                         // if image touched & bulb connected,
             data = new Uint8Array(1);
-            data[0] = (event.target.className=="Off") ? 0x1 : 0x0;                                          // set up switch toggle write 
-            ble.write(deviceID, light.service, light.onoff, data.buffer, app.onRead(data), app.onRWError);  // write switch characteristic; if good, goto: onRead
+            data[0] = (document.querySelector("svg").getAttribute("class")=="Off") ? 0x1 : 0x0;                   // set up switch toggle write 
+            ble.write(deviceID, light.service, light.onoff, data.buffer, app.onRead(data), app.onRWError);        // write switch characteristic; if good, goto: onRead
         }
     },
-    onPause: function() {                                                                                   // if user leaves app, stop BLE
-        ble.disconnect(deviceID,function(){deviceID="";});
+    onPause: function() {                                                                                         // if user leaves app, stop BLE
+        ble.disconnect(deviceID);
         ble.stopScan();
     },
-    onRWError: function() {                                                                                 // on error, try restarting BLE
+    onRWError: function() {                                                                                       // on error, try restarting BLE
         ble.isEnabled(deviceID,function(){},app.onDeviceReady);
         ble.isConnected(deviceID,function(){},app.onDeviceReady);
     }
 };
 
-app.initialize();                                                                                           // start the app
+app.initialize();                                                                                                 // start the app
