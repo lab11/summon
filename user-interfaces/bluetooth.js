77a98fc58639c9a1d8825f7aad39809a305b05f2
Thomas Zachariah <tzachari@umich.edu>
2016-04-06 21:10:16 -0400

    Update UI Bluetooth shim

--- user-interfaces/bluetooth.js
+++ user-interfaces/bluetooth.js
@@ -1,4 +1,7 @@
-/* vim: set sw=2 expandtab tw=80: */
+var PRTCOL = ["","","aaa:","aaas:","about:","acap:","acct:","cap:","cid:","coap:","coaps:","crid:","data:","dav:","dict:","dns:","file:","ftp:","geo:","go:","gopher:","h323:","http:","https:","iax:","icap:","im:","imap:","info:","ipp:","ipps:","iris:","iris.beep:","iris.xpc:","iris.xpcs:","iris.lwz:","jabber:","ldap:","mailto:","mid:","msrp:","msrps:","mtqp:","mupdate:","news:","nfs:","ni:","nih:","nntp:","opaquelocktoken:","pop:","pres:","reload:","rtsp:","rtsps:","rtspu:","service:","session:","shttp:","sieve:","sip:","sips:","sms:","snmp:","soap.beep:","soap.beeps:","stun:","stuns:","tag:","tel:","telnet:","tftp:","thismessage:","tn3270:","tip:","turn:","turns:","tv:","urn:","vemmi:","ws:","wss:","xcon:","xcon-userid:","xmlrpc.beep:","xmlrpc.beeps:","xmpp:","z39.50r:","z39.50s:","acr:","adiumxtra:","afp:","afs:","aim:","apt:","attachment:","aw:","barion:","beshare:","bitcoin:","bolo:","callto:","chrome:","chrome-extension:","com-eventbrite-attendee:","content:","cvs:","dlna-playsingle:","dlna-playcontainer:","dtn:","dvb:","ed2k:","facetime:","feed:","feedready:","finger:","fish:","gg:","git:","gizmoproject:","gtalk:","ham:","hcp:","icon:","ipn:","irc:","irc6:","ircs:","itms:","jar:","jms:","keyparc:","lastfm:","ldaps:","magnet:","maps:","market:","message:","mms:","ms-help:","ms-settings-power:","msnim:","mumble:","mvn:","notes:","oid:","palm:","paparazzi:","pkcs11:","platform:","proxy:","psyc:","query:","res:","resource:","rmi:","rsync:","rtmfp:","rtmp:","secondlife:","sftp:","sgn:","skype:","smb:","smtp:","soldat:","spotify:","ssh:","steam:","submit:","svn:","teamspeak:","teliaeid:","things:","udp:","unreal:","ut2004:","ventrilo:","view-source:","webcal:","wtai:","wyciwyg:","xfire:","xri:","ymsgr:","example:","ms-settings-cloudstorage:"];
+var SUFFIX = [".com/",".org/",".edu/",".net/",".info/",".biz/",".gov/",".com",".org",".edu",".net",".info",".biz",".gov"];
+var PREFIX = ["http://www.","https://www.","http://","https://","urn:uuid:"];
+
 var bluetooth = {};
 
 document.addEventListener("deviceready", function () {
@@ -7,167 +10,152 @@ document.addEventListener("deviceready", function () {
 
   bluetooth.scan = function(services, seconds, success, failure) {
     ble.scan(services, seconds, function (peripheral) {
-      translate_advertisement(peripheral, success);
+      parse(peripheral, success);
     }, failure);
   };
 
   bluetooth.startScan = function(services, success, failure) {
     ble.startScan(services, function (peripheral) {
-      translate_advertisement(peripheral, success);
+      parse(peripheral, success);
     }, failure);
   };
 
-  // create a common advertisement interface between iOS and android
+  bluetooth.connect = function(device_id, success, failure) {
+    ble.connect(device_id, function (peripheral) {
+      parse(peripheral, success);
+    }, failure);
+  };
+
+  // Create a common advertisement interface between iOS and android
   //  This format follows the nodejs BLE library, noble
   //  https://github.com/sandeepmistry/noble#peripheral-discovered
-  translate_advertisement = function (peripheral, success) {
-    var advertising = peripheral.advertising;
+  parse = function (peripheral, success) {
+    var advertisement = {};
 
-    // common advertisement interface is created as a new field
-    //  This format follows the nodejs BLE library, noble
-    //  https://github.com/sandeepmistry/noble#peripheral-discovered
-    peripheral.advertisement = {
-      localName: undefined,
-      txPowerLevel: undefined,
-      manufacturerData: undefined,
-      serviceUuids: [],
-      serviceData: [],
-      channel: undefined,       // ios only
-      isConnectable: undefined, // ios only
-      flags: undefined,         // android only
-    };
-
-    // this is a hack and only has to be in place until this code gets pulled
-    //  into the summon app. Since we use the same hack to decide which
-    //  cordova.js to load, it seems pretty saft to use it here too
     if (navigator.platform.startsWith("iP")) {
-
-      // we are on iOS (iPad, iPod, iPhone)
-      peripheral.advertisement.channel = advertising.kCBAdvDataChannel;
-      peripheral.advertisement.isConnectable = advertising.kCBAdvDataIsConnectable;
-
-      // directly copy fields as long as they exist
-      if (advertising.kCBAdvDataLocalName) {
-        peripheral.advertisement.localName = advertising.kCBAdvDataLocalName;
+      // Platform is iOS
+      for (var n in peripheral.advertising) {
+        advertisement[n[10].toLowerCase()+n.substr(11)] = peripheral.advertising[n];
       }
-      if (advertising.kCBAdvDataTxPowerLevel) {
-        peripheral.advertisement.txPowerLevel = advertising.kCBAdvDataTxPowerLevel;
+      if (peripheral.advertising.kCBAdvDataManufacturerData) {
+        advertisement.manufacturerData = new Uint8Array(peripheral.advertising.kCBAdvDataManufacturerData);
       }
-      if (advertising.kCBAdvDataManufacturerData) {
-        peripheral.advertisement.manufacturerData = new Uint8Array(advertising.kCBAdvDataManufacturerData);
+      if (peripheral.advertising.kCBAdvDataServiceUUIDs) {
+        delete advertisement.serviceUUIDs;
+        advertisement.serviceUuids = peripheral.advertising.kCBAdvDataServiceUUIDs;
       }
-      if (advertising.kCBAdvDataServiceUUIDs) {
-        peripheral.advertisement.serviceUuids = advertising.kCBAdvDataServiceUUIDs;
-      }
-
-      // format for service data is different
-      var serviceData = advertising.kCBAdvDataServiceData;
-      if (serviceData) {
-        for (var serviceDataUuid in serviceData) {
-          peripheral.advertisement.serviceData.push({
+      if (peripheral.advertising.kCBAdvDataServiceData) { // format for service data is different
+        advertisement.serviceData=[];
+        for (var serviceDataUuid in peripheral.advertising.kCBAdvDataServiceData) {
+          advertisement.serviceData.push({
             uuid: serviceDataUuid,
-            data: new Uint8Array(serviceData[serviceDataUuid]),
+            data: new Uint8Array(peripheral.advertising.kCBAdvDataServiceData[serviceDataUuid]),
           });
         }
       }
-
     } else {
-      // we are on android
-      var scanRecord = new Uint8Array(advertising);
+      // Platform is Android
+      var scanRecord = new Uint8Array(peripheral.advertising);
       var index = 0;
       while (index < scanRecord.length) {
-        // first is length of the field, length of zero indicates advertisement
-        //  is complete
+        // First is length of the field, length of zero indicates advertisement is complete
         var length = scanRecord[index++];
         if (length == 0) {
           break;
         }
 
-        // next is type of field and then field data (if any)
+        // Next is type of field and then field data (if any)
         var type = scanRecord[index];
         var data = scanRecord.subarray(index+1, index+length);
 
-        // determine data based on field type
+        // Determine data based on field type
         switch (type) {
           case 0x01: // Flags
-            peripheral.advertisement.flags = data[0] & 0xFF;
+            advertisement.flags = data[0] & 0xFF;
             break;
-
           case 0x02: // Incomplete List of 16-Bit Service UUIDs
           case 0x03: // Complete List of 16-Bit Service UUIDs
             for (var n=0; n<data.length; n+=2) {
-              peripheral.advertisement.serviceUuids.push(uuid(data.subarray(n,n+2)));
+              advertisement.serviceUuids = (advertisement.serviceUuids||[]).concat(uuid(data.subarray(n,n+2)));
             }
             break;
-
           case 0x04: // Incomplete List of 32-Bit Service UUIDs
           case 0x05: // Complete List of 32-Bit Service UUIDs
             for (var n=0; n<data.length; n+=4) {
-              peripheral.advertisement.serviceUuids.push(uuid(data.subarray(n,n+4)));
+              advertisement.serviceUuids = (advertisement.serviceUuids||[]).concat(uuid(data.subarray(n,n+4)));
             }
             break;
-
           case 0x06: // Incomplete List of 128-Bit Service UUIDs
           case 0x07: // Complete List of 128-Bit Service UUIDs
             for (var n=0; n<data.length; n+=16) {
-              peripheral.advertisement.serviceUuids.push(uuid(data.subarray(n,n+16)));
+              advertisement.serviceUuids = (advertisement.serviceUuids||[]).concat(uuid(data.subarray(n,n+16)));
             }
             break;
-
           case 0x08: // Short Local Name
           case 0x09: // Complete Local Name
-            peripheral.advertisement.localName = String.fromCharCode.apply(null,data);
+            advertisement.localName = String.fromCharCode.apply(null,data);
             break;
-
           case 0x0A: // TX Power Level
-            peripheral.advertisement.txPowerLevel = data[0] & 0xFF;
+            advertisement.txPowerLevel = data[0] & 0xFF;
             break;
-
           case 0x16: // Service Data
-            peripheral.advertisement.serviceData.push({
+            advertisement.serviceData = (advertisement.serviceData||[]).concat({
               uuid: uuid(data.subarray(0,2)),
               data: new Uint8Array(data.subarray(2)),
             });
             break;
-
+          case 0x24: // Bluetooth URI
+            advertisement.uri = PRTCOL[data[0]] + String.fromCharCode.apply(null, data.subarray(1));
+            peripheral.uri = advertisement.uri; // Attach URI if Bluetooth URI exists
+            break;
           case 0xFF: // Manufacturer Specific Data
-            peripheral.advertisement.manufacturerData = new Uint8Array(data);
+            advertisement.manufacturerData = new Uint8Array(data);
             break;
         }
 
-        // move to next advertisement field
+        // Shift to next advertisement field
         index += length;
       }
     }
 
-    // finished parsing, call originally intended callback
+    // Attach URI if Eddystone-URL exists
+    for (i in advertisement.serviceData) {
+      n = advertisement.serviceData[i];
+      if (n.uuid == "FEAA" || n.uuid == "FED8") {
+        peripheral.uriFrame = n.data[0]; // Frame type is the 1st byte
+        peripheral.uriTxPower = n.data[1]; // TX Power is 2nd byte
+        peripheral.uri = (PREFIX[n.data[2]] || String.fromCharCode(n.data[2])) + (function(){
+          for (var s = j = c = ''; (c=this[j++]) || c==0; s += SUFFIX[c] || String.fromCharCode(c)) {} 
+          return s;
+        }).call(n.data.subarray(3));
+      }
+    }
+
+    peripheral.advertisement = advertisement;
+
+    // Finished parsing, call originally intended callback
     success(peripheral);
   };
 
   // convert an array of bytes representing a UUID into a hex string
   //    Note that all arrays need to be reversed before presenting to the user
   uuid = function (id) {
-    if (id.length == 16) {
-      // 128-bit UUIDs should be formatted specially
-      return hex(id.subarray(12, 16)) + '-' +
-             hex(id.subarray(10, 12)) + '-' +
-             hex(id.subarray( 8, 10)) + '-' +
-             hex(id.subarray( 6,  8)) + '-' +
-             hex(id.subarray( 0,  6));
-
-    } else {
-        return hex(id);
+    id = id.reverse();
+    if (id.length <= 4) {
+      return hex(id);
+    } else if (id.length == 16) {
+      return hex(id.subarray( 0, 4)) + "-" + hex(id.subarray( 4, 6)) + "-" + hex(id.subarray( 6, 8)) +  "-" +
+             hex(id.subarray( 8,10)) + "-" + hex(id.subarray(10,16));
+    } else {  // invalid number of bytes
+      return "";
     }
   };
 
   // convert an array of bytes into hex data
-  //    assumes data needs to be in reverse order
-  hex = function (byte_array) {
-      var hexstr = '';
-      for (var i=(byte_array.length-1); i>=0; i--) {
-          hexstr += byte_array[i].toString(16).toUpperCase();
-      }
-      return hexstr;
-  }
+  hex = function (ab) {
+    return Array.prototype.map.call(ab,function(m){
+      return ("0"+m.toString(16)).substr(-2);
+    }).join('').toUpperCase();
+  };
 
 });
