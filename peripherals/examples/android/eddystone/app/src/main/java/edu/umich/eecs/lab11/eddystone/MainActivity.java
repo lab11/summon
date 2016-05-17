b7b0299f1b45fc89a2a79c54adad11e7a9b6c524
Thomas Zachariah <tzachari@umich.edu>
2016-05-16 20:24:05 -0400

    Better handling of URL shortening and Bluetooth errors in Eddystone app

--- peripherals/examples/android/eddystone/app/src/main/java/edu/umich/eecs/lab11/eddystone/MainActivity.java
+++ peripherals/examples/android/eddystone/app/src/main/java/edu/umich/eecs/lab11/eddystone/MainActivity.java
@@ -10,6 +10,8 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.os.ParcelUuid;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
@@ -27,13 +29,13 @@ public class MainActivity extends PreferenceActivity implements SharedPreference
     private SharedPreferences prefs;
     private BluetoothAdapter bleAdapter;
     private BluetoothLeAdvertiser bleAdvertiser;
-    private AdvertiseSettings.Builder settingsBuilder;
-    private AdvertiseData.Builder dataBuilder;
-    private String fullUrl="";
+    private String fullUrl = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("Advertise: Off");
         prefs = PreferenceManager.getDefaultSharedPreferences(this);
         prefs.registerOnSharedPreferenceChangeListener(this);
         getFragmentManager().beginTransaction().replace(android.R.id.content, new MainFragment()).commit();
@@ -61,11 +63,6 @@ public class MainActivity extends PreferenceActivity implements SharedPreference
         super.onResume();
         prefs = PreferenceManager.getDefaultSharedPreferences(this);
         prefs.registerOnSharedPreferenceChangeListener(this);
-
-        // Ensures Bluetooth is enabled on the device.
-        if (!bleAdapter.isEnabled()) bleAdapter.enable();
-
-        bleAdvertiser = bleAdapter.getBluetoothLeAdvertiser();
         advertise();
     }
 
@@ -75,18 +72,25 @@ public class MainActivity extends PreferenceActivity implements SharedPreference
     }
 
     private void advertise() {
-        runOnUiThread( new Runnable() { public void run() { setTitle("Advertise: " + prefs.getString("final_url","http://umich.edu")); }});
-        bleAdvertiser.stopAdvertising(advertiseCallback);
-        settingsBuilder = new AdvertiseSettings.Builder()
-                .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_BALANCED)
-                .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_HIGH)
-                .setConnectable(true).setTimeout(0);
-        dataBuilder = new AdvertiseData.Builder()
-                .addServiceUuid(shortUUID("FEAA"))
-                .addServiceData(shortUUID("FEAA"), toByteArray(prefs.getString("ad_value","0000")));
+        if (prefs.getBoolean("advertise_switch",false)) {
+            if (!bleAdapter.isEnabled() || bleAdvertiser==null) {
+                bleAdapter.enable();
+                bleAdvertiser = bleAdapter.getBluetoothLeAdvertiser();
+                new Handler(Looper.getMainLooper()).postDelayed(new Runnable(){public void run(){ advertise(); }}, 100);
+            } else {
+                runOnUiThread( new Runnable() { public void run() { setTitle("Advertise: " + prefs.getString("final_url","http://umich.edu")); }});
+                bleAdvertiser.stopAdvertising(advertiseCallback);
+                AdvertiseSettings.Builder settingsBuilder = new AdvertiseSettings.Builder()
+                        .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_BALANCED)
+                        .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_HIGH)
+                        .setConnectable(true).setTimeout(0);
+                AdvertiseData.Builder dataBuilder = new AdvertiseData.Builder()
+                        .addServiceUuid(shortUUID("FEAA"))
+                        .addServiceData(shortUUID("FEAA"), toByteArray(prefs.getString("ad_value", "0000")));
 
-        if (prefs.getBoolean("advertise_switch",false))
-            bleAdvertiser.startAdvertising(settingsBuilder.build(), dataBuilder.build(), advertiseCallback);
+                bleAdvertiser.startAdvertising(settingsBuilder.build(), dataBuilder.build(), advertiseCallback);
+            }
+        }
     }
 
     private AdvertiseCallback advertiseCallback = new AdvertiseCallback() {
@@ -97,9 +101,14 @@ public class MainActivity extends PreferenceActivity implements SharedPreference
         @Override
         public void onStartFailure(int errorCode) {
             super.onStartFailure(errorCode);
-            toastNotify("Failed to Advertise");
-            prefs.edit().putBoolean("advertise_switch",false).commit();
-            getFragmentManager().beginTransaction().replace(android.R.id.content, new MainFragment()).commit();
+            if (errorCode==3) {
+                bleAdapter.disable();
+                new Handler(Looper.getMainLooper()).postDelayed(new Runnable(){public void run(){ advertise(); }},100);
+            } else {
+                toastNotify("Failed to Advertise.");
+                prefs.edit().putBoolean("advertise_switch", false).apply();
+                getFragmentManager().beginTransaction().replace(android.R.id.content, new MainFragment()).commit();
+            }
         }
 
     };
@@ -124,13 +133,15 @@ public class MainActivity extends PreferenceActivity implements SharedPreference
         new Thread( new Runnable(){ public void run() {
             String pre = prefs.getString("protocol","http://");
             String url = prefs.getString("ip_text","umich.edu");
-            if (!prefs.getBoolean("advertise_switch", false) || !key.equals("advertise_switch")) {
-                if (bleAdvertiser != null) bleAdvertiser.stopAdvertising(advertiseCallback);
+            boolean on = prefs.getBoolean("advertise_switch", false);
+            if (!on || !key.equals("advertise_switch")) {
+                if (bleAdvertiser != null && bleAdapter.isEnabled()) bleAdvertiser.stopAdvertising(advertiseCallback);
                 runOnUiThread( new Runnable() { public void run() { setTitle("Advertise: Off"); }});
             }
-            if (!fullUrl.equals(pre + url)) {
+            if (on && !fullUrl.equals(pre + url)) {
                 try {
                     if (url.length() > 17) {
+                        runOnUiThread( new Runnable() { public void run() { setTitle("Shortening URL..."); }});
                         HttpURLConnection huc = (HttpURLConnection) (new URL("https://www.googleapis.com/urlshortener/v1/url?key=" + BuildConfig.API_KEY)).openConnection();
                         huc.setRequestMethod("POST");
                         huc.setDoOutput(true);
@@ -144,13 +155,16 @@ public class MainActivity extends PreferenceActivity implements SharedPreference
                         url = json.substring(json.indexOf(pre) + pre.length(), json.indexOf("\"", json.indexOf(pre)));
                     }
                     fullUrl = pre + prefs.getString("ip_text", "umich.edu");
-                    prefs.edit().putString("final_url", pre + url).commit();
-                    prefs.edit().putString("ad_value", "10BA" + (pre.equals("http://") ? "02" : "03") + toHex(url.getBytes())).commit();
+                    prefs.edit().putString("final_url", pre + url).apply();
+                    prefs.edit().putString("ad_value", "10BA" + (pre.equals("http://") ? "02" : "03") + toHex(url.getBytes())).apply();
                 } catch (Exception e) {
+                    toastNotify("Failed to shorten URL.\nCheck Internet connection.");
+                    prefs.edit().putBoolean("advertise_switch",false).apply();
+                    runOnUiThread(new Runnable(){public void run(){ getFragmentManager().beginTransaction().replace(android.R.id.content, new MainFragment()).commit(); }});
                     e.printStackTrace();
                 }
             }
-            if (prefs.getBoolean("advertise_switch", false))  advertise();
+            if (on) advertise();
         }}).start();
     }
 
@@ -173,13 +187,11 @@ public class MainActivity extends PreferenceActivity implements SharedPreference
 
     public String toHex(byte[] ba) {
         StringBuilder str = new StringBuilder();
-        for (int i = 0; i<ba.length; i++) str.append(String.format("%02X", ba[i]));
+        for (byte aBa : ba) str.append(String.format("%02X", aBa));
         return str.toString();
     }
 
-    public void toastNotify(String m) {
-        Toast.makeText(this, m, Toast.LENGTH_SHORT).show();
+    public void toastNotify(final String m) {
+        runOnUiThread( new Runnable() { public void run() { Toast.makeText(getApplicationContext(), m, Toast.LENGTH_SHORT).show(); }});
     }
-
-
 }
