c9dfa30f0afadffa0ab96e723022023a973f4ae2
Thomas Zachariah <tzachari@umich.edu>
2016-09-20 19:06:08 -0400

    Update caching method in iOS widget

--- mobile-app/src/ios/widget/TodayViewController.h
+++ mobile-app/src/ios/widget/TodayViewController.h
@@ -7,14 +7,14 @@
 #import <UIKit/UIKit.h>
 #import <CoreBluetooth/CoreBluetooth.h>
 
-NSMutableArray *devices;
 NSString * const PREFIX[] = {@"http://www.",@"https://www.",@"http://",@"https://",@"urn:uuid:"};
 NSString * const J2XUS[] = {@"6EKY8W",@"WRzp2g",@"qtn9V9",@"WRKqIy",@"2W2FTt",@"BA1zPM",@"8685Uw",@"hWTo8W",@"nCQV8C",@"sbMMHT",@"9aD6Wi",@"2ImXWJ",@"dbhGnF",@"3YACnH",@"449K5X",@"jEKPu9",@"xWppj1",@"Edukt0"};
 
-@interface TodayViewController : UIViewController <UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, CBCentralManagerDelegate, CBPeripheralDelegate>
+@interface TodayViewController : UIViewController <UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, CBCentralManagerDelegate>
 
-@property(nonatomic, weak) IBOutlet UICollectionView *collectionView;
-@property (nonatomic, strong) NSArray *dataArray;
+@property (nonatomic, weak  ) IBOutlet UICollectionView *collectionView;
+@property (nonatomic, strong) NSMutableArray *devices;
+@property (nonatomic, strong) NSMutableArray *archive;
 @property (nonatomic, strong) CBCentralManager *centralManager;
 
 @end
--- mobile-app/src/ios/widget/TodayViewController.m
+++ mobile-app/src/ios/widget/TodayViewController.m
@@ -17,13 +17,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 //    [[NSUserDefaults standardUserDefaults] removeObjectForKey:@"Devices"];
+    _devices = [[NSMutableArray alloc] init];
     _centralManager = [[CBCentralManager alloc] initWithDelegate:self queue:nil];
 }
 
 -(void)viewWillDisappear:(BOOL)animated {
     [_centralManager stopScan];
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    [defaults setObject:[NSKeyedArchiver archivedDataWithRootObject:devices] forKey:@"Devices"];
+    [defaults setObject:[NSKeyedArchiver archivedDataWithRootObject:_devices] forKey:@"Devices"];
     [defaults synchronize];
     [super viewWillDisappear:animated];
 }
@@ -33,10 +34,9 @@
     // If an error is encountered, use NCUpdateResultFailed
     // If there's no update required, use NCUpdateResultNoData
     // If there's an update, use NCUpdateResultNewData
-    NSData *archive = [[NSUserDefaults standardUserDefaults] objectForKey:@"Devices"];
-    if (devices == nil || devices.count==0) devices = [[NSMutableArray alloc] init];
-    if (archive != nil) devices = [NSKeyedUnarchiver unarchiveObjectWithData:archive];
-    [self.collectionView reloadData];
+    NSData *archdata = [[NSUserDefaults standardUserDefaults] objectForKey:@"Devices"];
+    if (archdata != nil) _archive = [NSKeyedUnarchiver unarchiveObjectWithData:archdata];
+    NSLog(@"%@",_archive);
     completionHandler(NCUpdateResultNewData);
 }
 
@@ -46,37 +46,50 @@
 }
 
 - (void)centralManager:(CBCentralManager *)central didDiscoverPeripheral:(CBPeripheral *)peripheral advertisementData:(NSDictionary *)advertisementData RSSI:(NSNumber *)RSSI {
-    NSLog(@"%@",peripheral);
+    NSLog(@"Peripheral Discovered: %@",peripheral);
     @try {
         NSData *data = advertisementData[CBAdvertisementDataServiceDataKey][[CBUUID UUIDWithString:@"FEAA"]];
         if (!data || data.length < 4) return;
         NSString *uri = [NSString stringWithFormat:@"%@%.*s",PREFIX[((char *)data.bytes)[2]],(uint)(data.length-3),((char *)data.bytes)+3];
         for (int i=0; i<((sizeof J2XUS)/(sizeof J2XUS[0])); i++) uri = [uri stringByReplacingOccurrencesOfString:[NSString stringWithFormat:@"goo.gl/%@",J2XUS[i]] withString:[NSString stringWithFormat:@"j2x.us/%@",J2XUS[i]]];
         if ([self itemExists:uri]) return;
-        NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:@"https://summon-caster.appspot.com/resolve-scan"]];
-        [request setHTTPMethod:@"POST"];
-        [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
-        [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
-        [request setHTTPBody:[[NSString stringWithFormat:@"{\"objects\":[{\"url\":\"%@\"}]}",uri] dataUsingEncoding:NSUTF8StringEncoding]];
-        [[[NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]] dataTaskWithRequest:request completionHandler:^(NSData *body, NSURLResponse *response, NSError *error) {
-            NSDictionary *md = [NSJSONSerialization JSONObjectWithData:body options:0 error:nil][@"metadata"][0];
-            if ([self itemExists:md[@"displayUrl"]]) return;
-            [devices addObject:[@{@"id":peripheral.identifier, @"name":peripheral.name?peripheral.name:@"Unnamed", @"ico":md[@"icon"], @"title":md[@"title"], @"uri":@[md[@"displayUrl"],md[@"id"]]} mutableCopy]];
-//            [self.collectionView setUserInteractionEnabled:YES];
+        NSMutableDictionary *item = [self checkArchive:uri];
+        if (item!=nil) {
+            [_devices addObject:item];
             [self.collectionView reloadData];
-        }] resume];
+        } else {
+            NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:@"https://summon-caster.appspot.com/resolve-scan"]];
+            [request setHTTPMethod:@"POST"];
+            [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+            [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+            [request setHTTPBody:[[NSString stringWithFormat:@"{\"objects\":[{\"url\":\"%@\"}]}",uri] dataUsingEncoding:NSUTF8StringEncoding]];
+            [[[NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]] dataTaskWithRequest:request completionHandler:^(NSData *body, NSURLResponse *response, NSError *error) {
+                @try {
+                    NSDictionary *md = [NSJSONSerialization JSONObjectWithData:body options:0 error:nil][@"metadata"][0];
+                    if ([self itemExists:md[@"displayUrl"]]) return;
+                    [_devices addObject:[@{@"id":peripheral.identifier, @"name":peripheral.name?peripheral.name:@"Unnamed", @"ico":md[@"icon"], @"title":md[@"title"], @"uri":@[md[@"displayUrl"],md[@"id"]]} mutableCopy]];
+        //            [self.collectionView setUserInteractionEnabled:YES];
+                    [self.collectionView reloadData];
+                } @catch(NSException *e){}
+            }] resume];
+        }
     } @catch(NSException *e){}
 }
 
 - (BOOL)itemExists:(NSString*)uri {
-    for (NSMutableDictionary *d in devices) if ([(NSArray*)d[@"uri"] containsObject:uri]) return true;
+    for (NSMutableDictionary *d in _devices) if ([(NSArray*)d[@"uri"] containsObject:uri]) return true;
     return false;
+}
+
+- (NSMutableDictionary*)checkArchive:(NSString*)uri {
+    for (NSMutableDictionary *d in _archive) if ([(NSArray*)d[@"uri"] containsObject:uri]) return d;
+    return nil;
 }
 
 #pragma mark - UICollectionView Datasource
 
 - (NSInteger)collectionView:(UICollectionView *)view numberOfItemsInSection:(NSInteger)section {
-    return [devices count];
+    return [_devices count];
 }
 
 - (NSInteger)numberOfSectionsInCollectionView: (UICollectionView *)collectionView {
@@ -86,7 +99,7 @@
 - (UICollectionViewCell *)collectionView:(UICollectionView *)cv cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     TodayViewCell *cell = (TodayViewCell*)[cv dequeueReusableCellWithReuseIdentifier:@"TodayViewCell" forIndexPath:indexPath];
     if (cell.icon.image != nil) return cell;
-    NSMutableDictionary *d = devices[indexPath.item]; NSLog(@"Peripheral: %@",d);
+    NSMutableDictionary *d = _devices[indexPath.item]; NSLog(@"Peripheral: %@",d);
     cell.name.text = d[@"title"];
     cell.icon.image = (d[@"icoData"]!=nil) ? [UIImage imageWithData:d[@"icoData"]] : [UIImage imageNamed:@"icon-ios-152.png"];
     [NSURLConnection sendAsynchronousRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:d[@"ico"]]] queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
