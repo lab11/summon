6c89ac0dccad80d6219472e63e5036ed3ec5a61e
Thomas Zachariah <tzachari@umich.edu>
2016-10-16 08:36:38 -0400

    Create data update function for GPS module in UI

--- user-interfaces/examples/signpost/www/js/index.js
+++ user-interfaces/examples/signpost/www/js/index.js
@@ -8,19 +8,30 @@ var app = {
         document.addEventListener("resume", app.onAppReady, false);
         document.addEventListener("pause", app.onPause, false);
 
-        var map = d3.select("#mod6").append("svg").attr("width",130).attr("height",65);
-        d3.json("js/map.json",function(e,geodata) { // Create GPS module map
-          if (e) return console.log(e);
-          map.selectAll("path")
-            .data(topojson.feature(geodata,geodata.objects.collection).features).enter().append("path")
-            .attr("d",d3.geo.path().projection(d3.geo.equirectangular().scale(20).translate([130/2,65/2])));
+        // Map for GPS module
+        var map = d3.select("#mod6");
+        var projection = d3.geo.equirectangular();
+        d3.json("js/map.json",function(e,geodata) {
+            if (e) return console.log(e);
+            map.selectAll("path")
+                .data(topojson.feature(geodata,geodata.objects.collection).features).enter().append("path")
+                .attr("d",d3.geo.path().projection(projection.scale(19).translate([120/2,65/2])));
+            lat=40; lon=173;
+            map.selectAll("line").remove()
+            map.selectAll("line")
+                .data([[[lon,-90],[lon,90]],[[-180,lat],[180,lat]]]).enter().append("line")
+                .attr("x1", function (d) { return projection(d[0])[0]; })
+                .attr("y1", function (d) { return projection(d[0])[1]; })
+                .attr("x2", function (d) { return projection(d[1])[0]; })
+                .attr("y2", function (d) { return projection(d[1])[1]; });
+            map.append("text").text(lat.toFixed(4) + "\u00B0 N " + lon.toFixed(4) + "\u00B0 W").attr("x",60).attr("y",80)
         });
     },
     // App Ready Event Handler
     onAppReady: function() {
         app.log("onAppReady");
         app.log("Checking if BLE is enabled...");
-        summon.bluetooth.isEnabled(app.onEnable);                                      // if BLE enabled, goto: onEnable
+        summon.bluetooth.isEnabled(app.onEnable);                                     // if BLE enabled, goto: onEnable
     },
     // App Paused Event Handler
     onPause: function() {
@@ -29,8 +40,8 @@ var app = {
     },
     // Bluetooth Enabled Callback
     onEnable: function() {
-        app.onPause();                                                                 // halt any previously running BLE processes
-        summon.bluetooth.startScan([], app.onDiscover, app.onAppReady);                // start BLE scan; if device discovered, goto: onDiscover
+        app.onPause();                                                                // halt any previously running BLE processes
+        summon.bluetooth.startScan([], app.onDiscover, app.onAppReady);               // start BLE scan; if device discovered, goto: onDiscover
         app.log("Searching");
     },
     // BLE Device Discovered Callback
