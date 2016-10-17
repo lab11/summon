cd7572280d3659202bdd10a7835b518aeba3531d
Thomas Zachariah <tzachari@umich.edu>
2016-10-17 17:50:04 -0400

    Implement press-to-zoom for modules in UI

--- user-interfaces/examples/signpost/www/js/index.js
+++ user-interfaces/examples/signpost/www/js/index.js
@@ -7,6 +7,8 @@ var app = {
         document.addEventListener("deviceready", app.onAppReady, false);
         document.addEventListener("resume", app.onAppReady, false);
         document.addEventListener("pause", app.onPause, false);
+        document.querySelector("#rect").addEventListener("click", app.onClick);
+        for (i=0; i<8; i++) document.querySelectorAll(".mod")[i].addEventListener("click", app.onClick);
 
         // Map for GPS module
         var map = d3.select("#mod6");
@@ -16,7 +18,7 @@ var app = {
             map.selectAll("path")
                 .data(topojson.feature(geodata,geodata.objects.collection).features).enter().append("path")
                 .attr("d",d3.geo.path().projection(projection.scale(19).translate([120/2,65/2])));
-            lat=40; lon=173;
+            lat=40; lon=73;
             map.selectAll("line").remove()
             map.selectAll("line")
                 .data([[[lon,-90],[lon,90]],[[-180,lat],[180,lat]]]).enter().append("line")
@@ -60,6 +62,16 @@ var app = {
                 app.log('Advertisement was not Signpost.');
             }
         }
+    },
+    // Module Click Event Handler
+    onClick: function() {
+        if (this.id!="rect" && document.body.style.transform=='') {
+            el = this.getBoundingClientRect();
+            x = window.innerWidth / 2 - el.width / 2 - el.left;
+            y = window.innerHeight / 2 - el.height / 2 - el.top;
+            s = Math.min(window.innerHeight,window.innerWidth) / (el.width + 20);
+            document.body.style.transform = 'scale('+s+') translate('+x+'px,'+y+'px)';
+        } else document.body.style.transform = '';
     },
     // Function to Log Text to Screen
     log: function(string) {
